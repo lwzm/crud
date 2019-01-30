a37f036ad182c4c5e51389e4319e05501d7dfbce
lwzm <lwzm@qq.com>
2019-01-30 15:11:42 +0800

    auto flush after logged in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { HashRouter as Router, Route, Link } from "react-router-dom"
 import { MenuList, MenuItem, Grid } from "@material-ui/core"
-import { Formik , Field} from 'formik'
+import { Formik, Field } from 'formik'
 import { get, post } from 'axios'
 import cfg from './cfg'
-import X from './c'
+import T from './table'
 
 const types = {
     "integer": "number",
@@ -29,60 +29,31 @@ const types = {
     "numeric": "number",
 }
 
-function Home() {
-    return <Formik
-        onSubmit={async (values, actions) => {
-            await post("/auth", values)
-            actions.setSubmitting(false)
-        }}
-    >
-    {({ handleSubmit }) => <form onSubmit={handleSubmit}>
-
-            <Field name="role" />
-            <button type="submit">Login</button>
-        </form>
-    }
-    </Formik>
-}
 
 function g(t) {
     return ({ history }) => {
         const s = localStorage.getItem(`resized:${t.table}`)
         const resized = s ? JSON.parse(s) : undefined
-        return <X hoc={true} history={history} resized={resized} {...t} />
+        return <T hoc={true} history={history} resized={resized} {...t} />
     }
 }
 
-function App({ tables }) {
-    return <Router>
-        <Grid container spacing={0}>
-            <Grid item xs={1}>
-                <MenuList className="menu-list" >
-                    {tables.map(({ table }) => <Link key={table} to={`/${table}`}>
-                        <MenuItem>{table}</MenuItem>
-                    </Link>)}
-                </MenuList>
-            </Grid>
-            <Grid item xs={11}>
-                <div style={{ width: 'auto' }}>
-                    <Route exact path="/" component={Home} />
-                    {tables.map((t, idx) => {
-                        return <Route key={idx} exact path={`/${t.table}`} component={g(t)} />
-                    })}
-                </div>
-            </Grid>
-        </Grid>
-    </Router>
-}
-
-export default class A extends React.Component {
+export default class App extends React.Component {
     state = {
         tables: []
     }
 
-    async componentWillMount() {
-        const fkRE = /fk table='([^']+)' column='([^']+)'/
+    componentWillMount() {
+        this.fetchDefinitions()
+    }
+
+    async fetchDefinitions() {
         const { data: { definitions } } = await get("/api/")
+        this.buildMenu(definitions)
+    }
+
+    buildMenu(definitions) {
+        const fkRE = /fk table='([^']+)' column='([^']+)'/
         const tables = {}
         for (const table in definitions) {
             const { properties } = definitions[table]
@@ -125,8 +96,42 @@ export default class A extends React.Component {
         Object.assign(cfg.tables, tables)
     }
 
+    renderHome = () => {
+        return <Formik
+            onSubmit={async (values, actions) => {
+                await post("/auth", values)
+                await this.fetchDefinitions()
+                actions.setSubmitting(false)
+            }}
+        >
+            {({ handleSubmit }) => <form onSubmit={handleSubmit}>
+                <Field name="role" />
+                <button type="submit">Login</button>
+            </form>
+            }
+        </Formik>
+    }
+
     render() {
-        return <App tables={this.state.tables} />
+        const { tables } = this.state
+        return <Router>
+            <Grid container spacing={0}>
+                <Grid item xs={1}>
+                    <MenuList className="menu-list" >
+                        {tables.map(({ table }) => <Link key={table} to={`/${table}`}>
+                            <MenuItem>{table}</MenuItem>
+                        </Link>)}
+                    </MenuList>
+                </Grid>
+                <Grid item xs={11}>
+                    <div style={{ width: 'auto' }}>
+                        <Route exact path="/" component={this.renderHome} />
+                        {tables.map((t, idx) => {
+                            return <Route key={idx} exact path={`/${t.table}`} component={g(t)} />
+                        })}
+                    </div>
+                </Grid>
+            </Grid>
+        </Router>
     }
 }
-
